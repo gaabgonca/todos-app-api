9897248efb6a54d6465a1d13da961a1a7b6555e3
[Gabriel GC] <[gaabgonca@gmail.com]>
2021-04-16 12:40:55 -0500

    First commit express and mongodb setup

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const colors = require('colors')
+
+const connectDB = require('./config/db')
+
+//Load environment variables
+
+dotenv.config({ path: './config/config.env' })
+
+// Connect to database
+connectDB()
+
+const app = express()
+
+// Body parser
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send({
+    text: 'Hello world',
+  })
+})
+
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+      .yellow.bold
+  )
+)
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  //Close server and exit process
+  server.close(() => {
+    process.exit(1)
+  })
+})
